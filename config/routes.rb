fe7beb05598f24619036a0455afe263e0f4d07e8
Peter Lemiszki <plemiszki@gmail.com>
2022-06-25 14:37:57 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "public#home"
+
+  resources :reservations, only: [:index, :show, :new]
+
+  namespace :api do
+    resources :reservations, only: [:show, :create, :update, :destroy]
+  end
+
 end
