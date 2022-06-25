b18fa3f79c4f6c5fdd6facef1ec981aa0c04ada3
Peter Lemiszki <plemiszki@gmail.com>
2022-06-25 14:36:51 -0400

    add reservation model

---
+++ app/models/reservation.rb
@@ -0,0 +1,12 @@
+class Reservation < ApplicationRecord
+
+  validates :user_id, :truck_id, :start_date, :end_date, presence: true
+
+  validate :valid_date_range?
+
+  def valid_date_range?
+    return unless start_date.present? && end_date.present?
+    errors.add(:end_date, "must be after Start date") unless end_date.after?(start_date)
+  end
+
+end
